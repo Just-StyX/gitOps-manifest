056ce0b589885b0f395668f9f8a0585344f6b884
Just-StyX <fjaccobian@gmail.com>
2025-09-04 22:04:07 -0400

    version 1.3 deployed

--- argo-cd-install.sh
+++ argo-cd-install.sh
@@ -6,3 +6,9 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # obtaining password
 kubectl get secret argocd-initial-admin-secret -n argocd -o yaml
 printf found_password | base64 --decode
+
+docker container inspect kind-control-plane --format '{{ .NetworkSettings.Networks.kind.IPAddress }}'
+
+ # adding istio
+istioctl install
+kubectl label namespace namespace-where-manifests-are-deployed istio-injection=enabled
